3dc3976e3150f47146a0bbddb0508e849641e427
Faezerezae <faezerezae234@gmail.com>
2025-04-20 17:12:11 +0330

    هندل useState

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
+import React, { useState } from 'react';
 import Link from 'next/link';
-import React, { useState } from 'react'
 
-function layoutAuth({
+function LayoutAuth({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [input, setInput] = useState("");
 
-  const [input, setInput] = useState("")
   return (
     <div>
-      layoutAuth
+      LayoutAuth
       <br />
       <Link className='mr-4' href={"/register"}>Register</Link>
       <Link className='mr-4' href={"/login"}>Login</Link>
       <Link className='mr-4' href={"/forget-password"}>ForgetPassword</Link>
       <br />
-      <input className='bg-gray-500' onChange={(e) => setInput(e.target.value)} type="text" value={input} />
+      <input
+        className='bg-gray-500'
+        onChange={(e) => setInput(e.target.value)}
+        type="text"
+        value={input}
+      />
       {children}
     </div>
-  )
+  );
 }
 
-export default layoutAuth
+export default LayoutAuth;
