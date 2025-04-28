2fa24d1030d0ddafe81396c2941b3ceb2f67ecfb
Faezerezae <faezerezae234@gmail.com>
2025-04-28 13:16:17 +0330

    fetch data catching daynamic & static

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Counter from './components/counter'
 import ServerComp from './components/serverComp'
-import axios from 'axios'
+// import axios from 'axios'
 import { IGetProduct } from '@/typescript/product';
 
 async function About() {
-  const { data } = await axios("http://localhost:8000/products");
+  // const { data } = await axios("http://localhost:8000/products");
+  const result = await fetch("http://localhost:8000/products",{
+    cache:"no-cache"
+  });
+  const data = await result.json();
   // console.log(data);
   return (
     <>
