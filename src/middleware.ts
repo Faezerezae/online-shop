e267ef9c86906f071b05631f529831ef1032dafb
Faezerezae <faezerezae234@gmail.com>
2025-04-28 16:32:30 +0330

    commit code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 // import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+// import type { NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
+// export function middleware(request: NextRequest) {
     // const url = new URL('/home', request.url);
     // const url = new URL("http://localhost:3000/contact-us");
     // return NextResponse.redirect(url);
-}
+// }
 
 // See "Matching Paths" below to learn more
 //اگر متچینگ پفز زیر نباشد میدلور در همه صفحات اجرا میشود
